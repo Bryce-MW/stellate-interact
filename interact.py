676d4eba111ea0ddd48ba6c9eaf054be9fe7cfa6
Bryce Wilson <bryce@brycemw.ca>
2021-01-17 09:31:08 +0000

    Add basics

--- interact.py
+++ interact.py
@@ -1,3 +1,22 @@
 #! /usr/bin/env python3
 
+from stellate_auth import password
+import os
+
 print("Hello!")
+print("Welcome to Stellate!")
+
+con = psycopg2.connect(database="stellate", user="bryce", host="10.29.33.3", password=password)
+cur = con.cursor()
+
+#SSH_ORIGINAL_COMMAND
+
+SSH_CMD = os.getenv("SSH_ORIGINAL_COMMAND")
+
+if SSH_CMD:
+  print("Automated connections are not implemented quite yet")
+  exit(0)
+
+print("\n Here are the hosts that you already have set up:")
+
+cur.execute("")
