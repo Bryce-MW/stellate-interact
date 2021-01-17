dacf841f8dd81f395ba5ade0e459f92c00fb1068
Bryce Wilson <bryce@brycemw.ca>
2021-01-17 01:52:18 -0800

    asdf

--- interact.py
+++ interact.py
@@ -2,6 +2,8 @@
 
 from stellate_auth import password
 import os
+import psycopg2
+import getpass
 
 print("Hello!")
 print("Welcome to Stellate!")
@@ -9,7 +11,7 @@ print("Welcome to Stellate!")
 con = psycopg2.connect(database="stellate", user="bryce", host="10.29.33.3", password=password)
 cur = con.cursor()
 
-#SSH_ORIGINAL_COMMAND
+# SSH_ORIGINAL_COMMAND
 
 SSH_CMD = os.getenv("SSH_ORIGINAL_COMMAND")
 
@@ -19,4 +21,9 @@ if SSH_CMD:
 
 print("\n Here are the hosts that you already have set up:")
 
-cur.execute("")
+cur.execute("""
+SELECT type, username, hostname, port FROM hosts INNER JOIN passwd_table ON "user"=uid WHERE username=%s
+""", (getpass.getuser(),))
+
+for i,line in enumerate(cur):
+  print(f" {i}. {line[0]} {line[1]}@{line[2]}:{line[3]}`")
